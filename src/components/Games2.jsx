14f049c2297825f9f162c11af36e530c016a7f65
autmku62622bcs31 <praveencm329@gmail.com>
2025-10-09 23:23:37 +0530

    first commit

--- src/components/Games2.jsx
+++ src/components/Games2.jsx
@@ -1,21 +1,6 @@
-//     <div className="card mb-3">
-//   <div className="row g-0">
-//     <div className="col-md-4">
-//       <img src={img} className="img-fluid rounded-start" alt="..."/>
-//     </div>
-//     <div className="col-md-8">
-//       <div className="card-body">
-//         <h5 className="card-title">Card title</h5>
-//         <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//         <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import img2 from '../assets/counter-strike.jpg'
 function Details() {
   const [game, setGame] = useState(null);
 
@@ -45,7 +30,7 @@ function Details() {
       <h1>{game.name}</h1>
       <p className='pubgpara'>Play PUBG: BATTLEGROUNDS for free! Land on strategic locations, loot weapons and supplies, and survive to become the last team standing across various, diverse Battlegrounds</p>
       <img
-        src={game.background_image}
+        src={img2}
         alt={game.name}
         style={{ width: "500px", borderRadius: "10px" }}
       />
